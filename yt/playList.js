d5746237f1554dea64b4d2e99049a36dd78317bb
Bojack <ben20415@gmail.com>
2021-06-25 21:04:55 +0800

    first commit

---
+++ yt/playList.js
@@ -0,0 +1,28 @@
+//YouTube Video ID
+let playList = [
+    "Yd_nsFJAXV4",
+    "R-fQPTwma9o",
+    "EH3zcuRQXNo",
+    "EO0abB6gJEw",
+    "x0Wa6ix51i0",
+    "_eu7EZIdLMM",
+    "U5tLGYIPGdQ",
+    "XzwVReweL_o",
+    "b5QXpUlEOyk",
+    "niElcLtli_Q",
+    "SGJIFBSCXgM"
+];
+//播放起訖秒數
+let playTime = [
+    [100, 103],
+    [110, 112],
+    [111, 114],
+    [32, 34],
+    [84, 89],
+    [47, 50],
+    [59, 61],
+    [59, 61],
+    [32, 35],
+    [49, 50],
+    [38, 40]
+];
